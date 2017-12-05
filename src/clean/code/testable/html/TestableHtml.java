5ce25d545345b4a07306dc712e65bcabbcd9003d
anduo <duoan@amazon.com>
2017-12-05 15:13:19 +0800

    extract common variable into construct method

--- src/clean/code/testable/html/TestableHtml.java
+++ src/clean/code/testable/html/TestableHtml.java
@@ -12,16 +12,17 @@ public class TestableHtml {
     private class TestableHtmlMaker {
         private PageData pageData;
         private boolean includeSuiteSetup;
+        private WikiPage wikiPage;
+        private StringBuffer buffer;
 
         public TestableHtmlMaker(PageData pageData, boolean includeSuiteSetup) {
             this.pageData = pageData;
             this.includeSuiteSetup = includeSuiteSetup;
+            this.wikiPage = pageData.getWikiPage();
+            this.buffer = new StringBuffer();
         }
 
         public String invoke() throws Exception {
-            WikiPage wikiPage = pageData.getWikiPage();
-            StringBuffer buffer = new StringBuffer();
-
             if (pageData.hasAttribute("Test")) {
                 if (includeSuiteSetup) {
                     WikiPage suiteSetup = PageCrawlerImpl.getInheritedPage(SuiteResponder.SUITE_SETUP_NAME, wikiPage);
